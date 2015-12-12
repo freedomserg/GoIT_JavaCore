ce2213b90130d56ada0879ee5e0d008febb48886
freedomserg <freedomserg97@gmail.com>
2015-12-12 17:05:49 +0200

    Commit Module 1

---
+++ src/Module1/FirstConsoleProgram.java
@@ -0,0 +1,7 @@
+package Module1;
+
+public class FirstConsoleProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
