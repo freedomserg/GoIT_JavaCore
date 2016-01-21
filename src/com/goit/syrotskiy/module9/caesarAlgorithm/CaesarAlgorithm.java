4ee4936167e2be3adb2a74b9b16c56067e500c01
freedomserg <freedomserg97@gmail.com>
2016-01-21 17:16:39 +0200

    commit module9 <CaesarAlgorithm>

--- src/com/goit/syrotskiy/module9/caesarAlgorithm/CaesarAlgorithm.java
+++ src/com/goit/syrotskiy/module9/caesarAlgorithm/CaesarAlgorithm.java
@@ -1,4 +1,25 @@
 package com.goit.syrotskiy.module9.caesarAlgorithm;
 
 public class CaesarAlgorithm {
+    private static String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()[]/?<>\'\"=-+*:;.,\t ";
+
+    public static String encode(String text, int key){
+        StringBuilder encodedContent = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            int index = alphabet.indexOf(symbol);
+            encodedContent.append(alphabet.charAt((index + key)%alphabet.length()));
+        }
+        return encodedContent.toString();
+    }
+
+    public static String decode(String text, int key){
+        StringBuilder decodedContent = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            int index = alphabet.indexOf(symbol);
+            decodedContent.append(alphabet.charAt((((index - key)%alphabet.length()) + alphabet.length())%alphabet.length()));
+        }
+        return decodedContent.toString();
+    }
 }
---
+++ src/com/goit/syrotskiy/module9/caesarAlgorithm/Runner.java
@@ -0,0 +1,47 @@
+package com.goit.syrotskiy.module9.caesarAlgorithm;
+
+import com.goit.syrotskiy.module3.files.*;
+import com.goit.syrotskiy.module8.collectionsPrinter.FileNameComparator;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Runner {
+    public static void main(String[] args) {
+        Set<File> fSet = new TreeSet<>(new FileNameComparator());
+        File aFile = new AudioFile("MySong");
+        File iFile = new ImageFile("MyPicture");
+        File tFile = new TextFile("MyText");
+
+        Directory dFile = new Directory("MyDirectory");
+        dFile.getDirectory().add(aFile);
+        dFile.getDirectory().add(iFile);
+        dFile.getDirectory().add(tFile);
+
+        fSet.add(aFile);
+        fSet.add(iFile);
+        fSet.add(tFile);
+        fSet.add(dFile);
+
+        String collectionToStringView = fSet.toString().replace("[", "").replace("]", "");
+        int key = receiveKey();
+        String encodeContent = CaesarAlgorithm.encode(collectionToStringView, key);
+        String decodeContent = CaesarAlgorithm.decode(encodeContent, key);
+
+        System.out.println("Initial content: " + collectionToStringView + "\n"
+                + "Encoded content: " + encodeContent + "\n"
+                + "Decoded content: " + decodeContent);
+    }
+
+    private static int receiveKey() {
+        Scanner reader = new Scanner(System.in);
+        int key = 0;
+        while(true) {
+            System.out.print("Please, enter a key (positive integer number): ");
+            key = reader.nextInt();
+            if (key > 0) break;
+        }
+        return key;
+    }
+}
