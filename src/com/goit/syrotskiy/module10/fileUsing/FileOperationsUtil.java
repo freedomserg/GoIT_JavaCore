944a181ec6d273b4ad530b903db4abdd73f4cea2
freedomserg <freedomserg97@gmail.com>
2016-01-31 18:21:03 +0200

    fix class FileOperationsUtil in module10

--- src/com/goit/syrotskiy/module10/fileUsing/FileOperationsUtil.java
+++ src/com/goit/syrotskiy/module10/fileUsing/FileOperationsUtil.java
@@ -5,8 +5,8 @@ import java.io.*;
 public class FileOperationsUtil {
 
     public static void uploadToFile(String text, String fileName) throws IOException {
-        try(DataOutputStream writer = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))) {
-            writer.writeUTF(text);
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write(text);
             System.out.println("Your message was saved to the file!");
         } catch (FileNotFoundException ex) {
             System.out.println("[ERROR]: such file" + " <" + fileName + "> " +  "doesn't exist!");
@@ -15,15 +15,15 @@ public class FileOperationsUtil {
 
     public static String readFile(String fileName) throws IOException {
         StringBuilder buffer = new StringBuilder();
-        try(DataInputStream reader = new DataInputStream(new BufferedInputStream(new FileInputStream(fileName)))) {
-            while(true) {
-                buffer.append(reader.readUTF());
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String input = reader.readLine();
+            while(input != null && !input.equals("")) {
+                buffer.append(input);
+                input = reader.readLine();
             }
         } catch (FileNotFoundException ex) {
             System.out.println("[ERROR]: such file" + " <" + fileName + "> " +  "doesn't exist!");
             throw ex;
-        } catch (EOFException ex) {
-            System.out.println("Reached end of file!");
         }
         return buffer.toString();
     }
